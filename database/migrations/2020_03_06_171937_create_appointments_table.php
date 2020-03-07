2ccc66b23b61b69dafda693cb9b2621e3f06d60c
BhavyaKesaria1993 <kesariabhavya@gmail.com>
2020-03-07 23:34:18 +0530

    initial commit

---
+++ database/migrations/2020_03_06_171937_create_appointments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('client',255);
+            $table->string('employee',255);   
+            $table->dateTime('start_time');
+            $table->dateTime('finish_time');
+            $table->float('price',10, 2);
+            $table->text('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
